d8674f508cfc7f81161bdcd9a8e7dd813d21cb38
martinmathew <martin.mathew.2k11@gmail.com>
2018-05-20 07:30:03 -0500

    commit

---
+++ java/src/com/may/Trees.java
@@ -0,0 +1,20 @@
+package com.may;
+
+public class Trees {
+	
+	
+	class Node<T>{
+		
+		private T value;
+		private Node<T> left;
+		private Node<T> right;
+		
+		public Node(T t) {
+			this.value = t;
+		}
+		
+		
+	}
+	
+
+}
