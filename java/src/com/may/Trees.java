71eb9e521f854e5f6c8796e1d6f358c4957413bb
martinmathew <martin.mathew.2k11@gmail.com>
2018-05-20 08:30:02 -0500

    commit

--- java/src/com/may/Trees.java
+++ java/src/com/may/Trees.java
@@ -1,9 +1,9 @@
 package com.may;
 
-public class Trees {
+public class Trees<T> {
 	
 	
-	class Node<T>{
+	 class  Node<T>{
 		
 		private T value;
 		private Node<T> left;
@@ -12,9 +12,51 @@ public class Trees {
 		public Node(T t) {
 			this.value = t;
 		}
+
+		public T getValue() {
+			return value;
+		}
+
+		public void setValue(T value) {
+			this.value = value;
+		}
+
+		public Node<T> getLeft() {
+			return left;
+		}
+
+		public void setLeft(Node<T> left) {
+			this.left = left;
+		}
+
+		public Node<T> getRight() {
+			return right;
+		}
+
+		public void setRight(Node<T> right) {
+			this.right = right;
+		}
 		
 		
 	}
 	
 
+	
+	
+	public  Node<T>  mirror(Node<T> root) {
+		if(root!=null) {
+		 root.setLeft(mirror(root.right));
+		 root.setRight(mirror(root.left));
+		 return root;
+		}
+		else {
+			return null;
+		}
+	}
+	
+	
+	
+	public 
+	
+	
 }
