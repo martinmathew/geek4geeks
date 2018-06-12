ecda22a77068ebf893db8fe02c19645d80b1a444
martinmathew <martin.mathew.2k11@gmail.com>
2018-06-12 06:00:04 -0500

    commit

---
+++ python/src/tree/Tree.py
@@ -0,0 +1,31 @@
+class Tree:
+    class Node:
+
+        def __init__(self,val):
+            self.left = None
+            self.right = None
+            self.val = val
+
+
+    def insert(self,arr):
+        for num in arr:
+            if self.root is None:
+                self.root = self.Node(num)
+            else:
+                temp = self.root
+                parent = None
+                while temp is not  None:
+                    parent = temp
+                    if temp.val <= num:
+                        temp = temp.right
+                    else:
+                        temp = temp.left
+
+                new_node = self.Node(num)
+                if parent.val <=num:
+                    parent.right = new_node
+                else:
+                    parent.left = new_node
+
+if __name__ == "__main__":
+    nums = [10,5,]
