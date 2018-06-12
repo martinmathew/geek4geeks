c9943089dc8242a7a4e46dfe7dd242489317a2e8
CORP\mmathew <mmathew@securustechnologies.com>
2018-06-12 16:37:16 -0500

    commit

--- gitcommit.bat
+++ gitcommit.bat
@@ -1,13 +1,13 @@
 
 cd D:\SourceCode\Securus\geek4geeks
-git pull origin master
-git add .
+git -c http.sslVerify=false pull origin master
+git -c http.sslVerify=false add .
 
 
 
-git commit -m "commit"
+git -c http.sslVerify=false commit -m "commit"
 
 
 
-git push origin master
+git -c http.sslVerify=false push origin master
 
