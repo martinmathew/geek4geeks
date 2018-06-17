6b2fefb0278d62a89db3235144fd932ff9325d18
martinmathew <martin.mathew.2k11@gmail.com>
2018-06-17 07:00:02 -0500

    commit

--- python/src/arrs/Miscellanious.py
+++ python/src/arrs/Miscellanious.py
@@ -1,3 +1,16 @@
 def longest_prefix(strs):
     index = 0
-    
+
+    prefix = ""
+    for index in range(len(min(strs,key=len))):
+        chr = strs[0][index]
+        for i in range(len(strs)):
+            if strs[i][index] != chr:
+                return prefix
+
+
+        prefix = prefix + chr
+    return prefix
+
+if __name__ == "__main__":
+    print(longest_prefix(["abcd", "abbd", "abdy", "az"]))
